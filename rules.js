26c421a51594e0c7fe0f34901df39c2f946687a6
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2019-10-10 16:37:02 +0530

    #7 | added endline count limit

--- rules.js
+++ rules.js
@@ -4,4 +4,5 @@ module.exports = _.merge({},
     require('./registration/rules/registrationFormHandler'),
     require('./psychometricformat/psychometricCalculations.js'),
     require('./baseline/baselineHandler.js'),
+    require('./endline/endlineHandler.js')
 );
